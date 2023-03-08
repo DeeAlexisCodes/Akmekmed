b4a6b28b22e31d79442834c0f2d289bbe2bafbb1
DeeAlexisCodes <dayemiarchibong@gmail.com>
2023-03-07 22:37:33 -0800

    new

--- src/sections/contacts/data.js
+++ src/sections/contacts/data.js
@@ -6,7 +6,7 @@ import {AiFillPhone} from 'react-icons/ai'
 const data = [
     {id: 1, icon: <CiLocationOn/>, title: 'LOCATION', desc: '82 Akpaube Street (Unity house) off Aka-Etinan Road, & off IBB Way, Uyo, AKS.'},
     {id: 2, icon: <BsFillPeopleFill/>, title: 'OPEN', desc: 'Weekdays: 7:30am - 6:30pm & Weekends: 9:00am - 2:00pm'},
-    {id: 3, icon: <AiOutlineMail/>, title: 'EMAIL', desc: 'akmekmed82@gmail.com.'},
+    {id: 3, icon: <AiOutlineMail/>, title: 'EMAIL', desc: 'admin@akmekmed.com; akmekmed82@gmail.com'},
     {id: 4, icon: <AiFillPhone/>, title: 'EMERGENCY', desc: '+234 813 666 8800; +234 803 582 3348'},
 ]
 
